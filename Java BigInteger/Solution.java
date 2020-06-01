5efc0bdf1988ec2803cee86c689c44f6aaeea143
Mayank Dawar <mayankdawar29@gmail.com>
2020-06-01 18:12:25 +0530

    Create Solution.java

---
+++ Java BigInteger/Solution.java
@@ -0,0 +1,28 @@
+/*In this problem, you have to add and multiply huge numbers! These numbers are so big that you can't contain them in any ordinary data types like a long integer.
+Use the power of Java's BigInteger class and solve this problem.
+Input Format
+There will be two lines containing two numbers, a and b.
+Output Format
+Output two lines. The first line should contain a+b, and the second line should contain a*b. Don't print any leading zeros.*/
+
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s = new Scanner(System.in);
+        String s1 = s.nextLine();
+        String s2 = s.nextLine();
+
+        BigInteger big1,big2;
+        big1 = new BigInteger(s1);
+        big2 = new BigInteger(s2);
+
+        System.out.println(big1.add(big2));
+        System.out.println(big1.multiply(big2));
+    }
+}
